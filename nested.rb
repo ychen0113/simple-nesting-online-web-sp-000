4e0b4e0420f48d0b75bc3c7f7162a22e2354ca89
Yiliang Chen <yiliangchen0113@gmail.com>
2020-10-30 02:45:10 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -110,7 +110,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-    programmer_hash[:alan_kay][:known_for] = [alans_new_info]
+    programmer_hash[:alan_kay][:known_for] = alans_new_info
     programmer_hash[:alan_kay][:known_for]
      
 end
@@ -132,6 +132,6 @@ def adding_to_dennis
         }
      }
      
-programmer_hash[:dennis_ritchie][:languages] << ["Assembly"]
+programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 programmer_hash[:dennis_ritchie][:languages]
 end
