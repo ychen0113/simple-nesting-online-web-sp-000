29c410c322df73d4cc620723d28d8563b22af26a
Yiliang Chen <yiliangchen0113@gmail.com>
2020-10-30 02:01:22 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,6 +15,8 @@ def hopper
           :languages => ["C"]
         }
      }
+     
+  programmer_hash[:grace_hopper]
 
 
 end
@@ -37,6 +39,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+    programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +59,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+  programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
@@ -77,6 +82,10 @@ def adding_matz
         :dennis_ritchie => {
           :known_for => "Unix",
           :languages => ["C"]
+        }
+        :yukihiro_matsumoto => {
+          :known_for => "Ruby",
+          :languages => ["LISP", "C"]
         }
      }
 
@@ -101,7 +110,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
+    programmer_hash[:alan_kay][:known_for] = alans_new_info
      
 end
 
@@ -121,5 +130,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+programmer_hash[:dennis_ritchie][:languages]
 end
