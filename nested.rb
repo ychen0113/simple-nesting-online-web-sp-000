7f2c01e4c0eb37cd42d121abc6b7432a4a81c668
Yiliang Chen <yiliangchen0113@gmail.com>
2020-10-30 02:33:04 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -131,6 +131,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+     x = "Assembly"
+programmer_hash[:dennis_ritchie][:languages] << x
 programmer_hash[:dennis_ritchie][:languages]
 end
